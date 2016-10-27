f02ed74e4c2b83aff43846cf2c5e8e9ff9bcb063
Juan Luna Bejarano <jualunab@coboglbw01234.global.publicisgroupe.net>
2016-10-27 15:29:25 -0500

    Object Constructor

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+  setTimeout(function(numero){
+    console.log("Hola");
+  }, 2000);
+
   var testInterval = function() {
     counter ++;
     console.log(counter);
@@ -10,3 +14,11 @@ $(document).ready(function(){
   var test = setInterval(testInterval, 2000), counter = 0;
   test();
 });
+
+// Object Constructor
+function Programas (nombre, duracion, calificacion) {
+  this.name = nombre;
+  this.duracion = duracion;
+  this.calificacion = calificacion;
+}
+var Primero = new Programas("Diseño Gráfico", 8, "A+");
