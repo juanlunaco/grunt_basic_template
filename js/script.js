a65cdf04e7a8ba810557ce9c0a1332d2f7c2ee48
Juan Luna Bejarano <jualunab@coboglbw01234.global.publicisgroupe.net>
2016-10-27 16:03:39 -0500

    Correccion Javascript

--- js/script.js
+++ js/script.js
@@ -3,16 +3,16 @@ $(document).ready(function(){
     console.log("Hola");
   }, 2000);
 
-  var testInterval = function() {
-    counter ++;
-    console.log(counter);
-
-    if (counter == 10){
-        clearInterval(test);
-    }
-  };
-  var test = setInterval(testInterval, 2000), counter = 0;
-  test();
+  // var testInterval = function() {
+  //   counter ++;
+  //   console.log(counter);
+  //
+  //   if (counter == 10){
+  //       clearInterval(test);
+  //   }
+  // };
+  // var test = setInterval(testInterval, 2000), counter = 0;
+  // test();
 });
 
 // Object Constructor
@@ -22,3 +22,17 @@ function Programas (nombre, duracion, calificacion) {
   this.calificacion = calificacion;
 }
 var Primero = new Programas("Diseño Gráfico", 8, "A+");
+
+// Call and Apply
+var persona = {
+  name : "Juan",
+  age : 22
+};
+function presentacion (nuevo){
+  console.log("Hola, " + nuevo + "," + " mi nombre es " + this.name + ", tengo " + this.age + " años.");
+}
+presentacion.call(persona, "Carlos");
+
+// Date Object
+var fecha = new Date();
+console.log(fecha);
